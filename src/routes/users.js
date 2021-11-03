126e23886c4eddf82c4bc547c5f432f2010bdc30
Joan Cortes <js.199911@gmail.com>
2021-11-03 12:07:34 -0500

    Crear metodo post y validacion password

--- src/routes/users.js
+++ src/routes/users.js
@@ -8,4 +8,29 @@ router.get('/users/signup', (req, res) => {
     res.render('users/signup');
 });
 
+router.post('/users/signup', (req, res) => {
+    const { name, email, password, confirm_password } = req.body;
+    const errors = [];
+
+    /* Validar campos vacios */
+    if (name.length<=0) {
+        errors.push({text: "Please insert your name"});
+    }
+    if (email.length<=0) {
+        errors.push({text: "Please insert your email"});
+    }
+    /* Validar contraseñas iguales */
+    if (password != confirm_password) {
+        errors.push({ text: 'Password do not match' });
+    }
+    if (password.length > 4) {
+        errors.push({ text: 'Password must be at least 4 characters' });
+    }
+    if (errors.length > 0) {
+        res.render('users/signup', { errors, name, email, password, confirm_password })
+    } else {
+        res.send('ok')
+    }
+});
+
 module.exports = router;
